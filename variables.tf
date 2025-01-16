189d60f8ae355f1d2600fd1925103aa2dee93c86
lamberthchance1 <lamberthchance1@gmail.com>
2025-01-16 11:45:01 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,6 @@ variable "terraform_cloud_organization" {
 
 variable "terraform_cloud_default_project_id" {
   description = "Terraform default project id"
-  type = string
-  default = "prj-JLaygAbry8tJDsYr"
+  type        = string
+  default     = "prj-JLaygAbry8tJDsYr"
 }
