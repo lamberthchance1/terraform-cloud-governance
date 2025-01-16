28e6c6d2e7412805ee25426dae308a1e1f2e387a
lamberthchance1 <lamberthchance1@gmail.com>
2025-01-16 11:25:41 -0600

    Update terraform_cloud_teams.tf

--- terraform_cloud_teams.tf
+++ terraform_cloud_teams.tf
@@ -1,4 +1,10 @@
-resource "tfe_team" "test" {
-  name         = "sample-team"
+# resource "tfe_team" "test" {
+#  name         = "sample-team"
+#  organization = var.terraform_cloud_organization
+#}
+
+resource "tfe_workspace" "test" {
+  name         = "sample-workspace"
   organization = var.terraform_cloud_organization
+  tag_names    = ["test", "app"]
 }
